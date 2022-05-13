51ce8b092501da8d88bd84d8c4ff7b55e2297289
ssanthu81 <101133033+ssanthu81@users.noreply.github.com>
2022-05-13 09:02:41 -0500

    Add files via upload

---
+++ enums/EmptyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace enums
+{
+
+		public enum days 
+		{
+			Monday, tuesday, wednesday,thursday,friday
+		}
+
+	class test
+	{
+		static void Main(string[] args)
+		{
+			days d = 0;
+			Console.WriteLine(d);
+			Console.ReadLine();
+		}
+
+
+	}
+	}
+
+
---
+++ enums/Program.cs
@@ -0,0 +1,8 @@
+﻿class test
+{
+    static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine("hello");
+    }
+}
